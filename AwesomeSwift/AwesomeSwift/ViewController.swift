854c97dca7e57c5bbf994a7c89cf48da832569a4
OkamotoHaru <truevr.lisbeth@gmail.com>
2019-08-04 14:20:26 +0900

    ## 対応 * プロジェクト作成

---
+++ AwesomeSwift/AwesomeSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeSwift
+//
+//  Created by masato on 2019/08/01.
+//  Copyright © 2019年 FreeNerdLaughed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
