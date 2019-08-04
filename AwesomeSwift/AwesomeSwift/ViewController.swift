12738f317d8a9a3a0f905bd0f4be4faacde4856f
OkamotoHaru <truevr.lisbeth@gmail.com>
2019-08-04 14:23:29 +0900

    ## 対応 * シミュレーター上でhello worldを表示 * コメントの書き方を表示 * ログの出し方を表示

--- AwesomeSwift/AwesomeSwift/ViewController.swift
+++ AwesomeSwift/AwesomeSwift/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /* 範囲コメント */
+        // １行コメント
+        
+        print("Hello World")
     }
 
 
